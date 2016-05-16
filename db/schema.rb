ace487a1ee56b42aa2460f03b3c0a93c6ee0f34c
turbio <turbio@masonclayton.com>
2016-05-16 05:03:43 -0700

    category table for skills

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516114031) do
+ActiveRecord::Schema.define(version: 20160516120025) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
