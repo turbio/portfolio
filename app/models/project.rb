b494bd4661f5e7fd6bda21a40ab1f561bb70b42d
turbio <turbio@masonclayton.com>
2016-05-16 12:46:26 -0700

    attribute validation didn't seem to work

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,12 +1,11 @@
 class Project < ActiveRecord::Base
 	#correctly capitalize the names of projects
 	def name
-		read_attribute(:name).split.map do |w| w.capitalize end.join ' ' if has_attribute? :name
-
+		read_attribute(:name).split.map do |w| w.capitalize end.join ' ' if read_attribute(:name)
 	end
 
 	#capitalize first letter and add period
 	def description
-		read_attribute(:description).capitalize + '.' if has_attribute? :description
+		read_attribute(:description).capitalize + '.' if read_attribute(:description)
 	end
 end
