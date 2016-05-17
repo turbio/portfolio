5f7a8eb6cdfb86ead8d7d996aa1bf5fe6ad2833b
turbio <turbio@masonclayton.com>
2016-05-17 08:36:56 -0700

    kill section general outline

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,7 +1,7 @@
 module MainHelper
 	def category_tree(top_categories)
 		top_categories.map do |category|
-			content_tag(:ul, content_tag(:h2, category.name) + if category.subcategories.length > 0
+			content_tag(:ul, content_tag(:h3, category.name) + if category.subcategories.length > 0
 				category_tree(category.subcategories)
 			else
 				category.skills.map do |skill| content_tag(:li, skill) end.join;
